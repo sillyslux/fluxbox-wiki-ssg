35d7772e4d8abacfb646f50a3b5a76591fa9e832
sillyslux <sillyslux@net.hr>
2017-05-08 05:38:26 +0200

    add show/hide button for toc

--- _pages/WikiPages/_template.jsx
+++ _pages/WikiPages/_template.jsx
@@ -1,11 +1,29 @@
+/* global localStorage */
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import pageInfo from 'utils/pageInfo'
 import styles from 'css/wiki-page.module.sass'
-import { Nav, Panel, Grid, Col, Row } from 'react-bootstrap-externaljs'
+import { Nav, Panel, Grid, Col, Row, OverlayTrigger, Button, Glyphicon, Tooltip } from 'react-bootstrap-externaljs'
 import 'css/toc.sass'
 
 export default class WikiPage extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      showToc: true,
+    }
+  }
+
+  componentDidMount () {
+    const oldState = JSON.parse(localStorage.getItem('showToc'))
+    this.setState({ showToc: oldState===null ? true : oldState })
+  }
+
+  toggleToc () {
+    localStorage.setItem('showToc', !this.state.showToc)
+    this.setState({ showToc: !this.state.showToc })
+  }
+
   render () {
     const { page } = pageInfo(this.props.location.pathname)
 
@@ -13,10 +31,10 @@ export default class WikiPage extends Component {
       <Grid className={styles['wiki-page']}>
         <Row>
           <Panel>
-            <Col md={9}>
+            <Col md={this.state.showToc ? 9 : 12}>
               {this.props.children}
             </Col>
-            <Col bsClass="sidebar sidebar-right col" md={3}>
+            <Col className={this.state.showToc ? '' : 'hideToc'} bsClass="sidebar sidebar-right col" md={3}>
               <Nav
                 id="spytoc"
                 bsStyle="pills"
@@ -26,6 +44,11 @@ export default class WikiPage extends Component {
                 data-offset-bottom="120"
                 data-offset-top="38"
               />
+              <OverlayTrigger container={this.tocButton} placement="left" delayShow={1000} overlay={<Tooltip id="user-menu">{this.state.showToc ? 'hide' : 'show'}<br />Table of Contents</Tooltip>}>
+                <Button bsStyle="link" bsSize="sm" onClick={e => this.toggleToc(e)} ref={(c) => { this.tocButton = c }}>
+                  <Glyphicon glyph={this.state.showToc ? 'eye-close' : 'menu-hamburger'} />
+                </Button>
+              </OverlayTrigger>
             </Col>
           </Panel>
         </Row>
