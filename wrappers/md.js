db0f5b72dd3fa8b4c76318c0e43e589f581f4ee2
sillyslux <sillyslux@net.hr>
2017-05-08 13:56:38 +0200

    fix new github relative markdown links

--- wrappers/md.js
+++ wrappers/md.js
@@ -2,18 +2,24 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { config, pages } from 'config'
+import { prefixLink } from 'gatsby-helpers'
 
 import style from 'css/markdown.module.sass'
 
 export default class Markdown extends Component {
   render () {
     const post = this.props.route.page.data
-    post.body = post.body.replace(/href="\/([^"]+)"/g, (attr, link) => {
+    post.body = post.body.replace(/href="([^"]+)"/g, (attr, link) => {
       const hashed = link.match(/(.+)#(.+)/)
       const path = (hashed && hashed[1]) || link
-      const page = pages.find(p => p.requirePath === path)
+      const page = pages.find(p =>
+        p.requirePath === path ||
+        p.path === path ||
+        (p.requirePath && prefixLink(p.requirePath) === path) ||
+        (p.path && prefixLink(p.path) === path)
+      )
       let fixedLink = page && page.path
-      fixedLink = fixedLink && (hashed ? `href="${fixedLink}#${hashed[2]}"` : `href="${fixedLink}""`)
+      fixedLink = fixedLink && (hashed ? `href="${prefixLink(fixedLink)}#${hashed[2]}"` : `href="${prefixLink(fixedLink)}""`)
       return fixedLink || attr
     })
     return (
